269fff52216d08c42eead6cc0675d9f23efc5f3d
DEVgrown <groown5@gmail.com>
2023-03-21 15:17:42 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,6 +2,14 @@
 #include "6-abs.c"
 #include <stdio.h>
 
+/**
+ * print_to_98 - function to print natural numbers
+ *
+ * @n: function oarameter
+ *
+ * Return: always 0
+ */
+
 void print_to_98(int n)
 {
 	int i;
