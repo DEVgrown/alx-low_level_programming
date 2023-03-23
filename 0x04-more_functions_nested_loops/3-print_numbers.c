dcdc512185130a1bf5d8a21462ee930fde1f8071
DEVgrown <groown5@gmail.com>
2023-03-23 16:23:35 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,33 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
-*Return: void
-*/
+<<<<<<< HEAD
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+/**
+		=======
+ * print_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
 
 void print_numbers(void)
 {
-	char c;
+	int number;
 
-	for (c = '0'; c <= '9'; c++)
+	for (number = '0'; number <= '9'; number++)
 	{
-	_putchar(c);
+		_putchar(number);
 	}
 	_putchar('\n');
 }
