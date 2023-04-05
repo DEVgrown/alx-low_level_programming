562b44e1f0b7e012b7ac5334dd17204f9798f596
DEVgrown <groown5@gmail.com>
2023-04-05 13:27:15 -0700

    corrections

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,18 +1,28 @@
 #include "main.h"
 /**
- * char
- * @s* : special char
- * Return: special character
+ * wildcmp
+ * @s1 : string
+ * Return: int value
  */
 int wildcmp(char *s1, char *s2) {
-        if (*s1 == '\0' && *s2 == '\0') {
-        return 1;
-    }
-    if (*s2 == '*') {
-        return (wildcmp(s1, s2+1) || (*s1 != '\0' && wildcmp(s1+1, s2)));
-    }
-        if (*s1 == *s2) {
-        return wildcmp(s1+1, s2+1);
-    }
-    return 0;
+	if (*s1 == '\0' && *s2 == '\0') {
+	return 1;
+	}
+/**
+ * char
+ * @s2 : try matching
+ * Return : int value
+ */
+	if (*s2 == '*') {
+	return (wildcmp(s1, s2+1) || (*s1 != '\0' && wildcmp(s1+1, s2)));
+	}
+/**
+ *
+ * @s1 == @s2
+ * Return : int value
+ */
+	if (*s1 == *s2) {
+	return wildcmp(s1+1, s2+1);
+	}
+	return 0;
 }
